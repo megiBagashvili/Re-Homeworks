9d68f82e0de4b84295cdb1cb2ecafbae467e23ab
megiBagashvili <megibagashvili2004@gmail.com>
2025-09-23 21:33:34 +0400

    completed task 2

--- homework5/hw5.js
+++ homework5/hw5.js
@@ -1,8 +1,24 @@
 //task 1
-function deleteSpecifiedParam(obj, prop){
+function deleteSpecifiedParam(obj, prop) {
     delete obj[prop];
     return obj;
 }
 
-let obj = {category: 'Cake', icing: 'Chocolate', serving: 8};
+let obj = { category: 'Cake', icing: 'Chocolate', serving: 8 };
 console.log(deleteSpecifiedParam(obj, 'icing'));
+
+//task 2
+function declareLeaderBoard(arr) {
+    let newArr = arr.sort((a, b) => b.score - a.score)
+    for (let stud in newArr) {
+        newArr[stud].rank = +stud + 1;
+    }
+    return newArr;
+}
+
+let students = [
+    { name: "Ana", score: 50 },
+    { name: "Nika", score: 80 },
+    { name: "Luka", score: 70 }
+]
+console.log(declareLeaderBoard(students));
