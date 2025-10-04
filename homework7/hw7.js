c3467df872149b2df11636ca9e1bdadd920f9af3
megiBagashvili <megibagashvili2004@gmail.com>
2025-10-04 21:47:15 +0400

    HW completed

--- homework7/hw7.js
+++ homework7/hw7.js
@@ -70,7 +70,6 @@ getProductsWithPriceAbove10().then(products => {
 });
 
 //task 4
-
 async function getWebDevelopers() {
   try {
     const response = await fetch("https://dummyjson.com/users");
@@ -93,3 +92,36 @@ async function getWebDevelopers() {
 }
 
 getWebDevelopers();
+
+//task 5
+async function fetchAllDummyJson() {
+  const endpoints = {
+    recipes:  "https://dummyjson.com/recipes",
+    comments: "https://dummyjson.com/comments",
+    todos:    "https://dummyjson.com/todos",
+    quotes:   "https://dummyjson.com/quotes"
+  };
+
+  const entries = Object.entries(endpoints);
+  const fetchPromises = entries.map(([key, url]) =>
+    fetch(url)
+      .then(res => {
+        if (!res.ok) throw new Error(`${key} fetch failed: ${res.status} ${res.statusText}`);
+        return res.json();
+      })
+      .then(json => [key, json])
+  );
+  const keyValuePairs = await Promise.all(fetchPromises);
+  return Object.fromEntries(keyValuePairs);
+}
+
+fetchAllDummyJson()
+  .then(({ recipes, comments, todos, quotes }) => {
+    console.log('recipes:', recipes);
+    console.log('comments:', comments);
+    console.log('todos:', todos);
+    console.log('quotes:', quotes);
+  })
+  .catch(err => {
+    console.error('One or more requests failed:', err);
+  });
