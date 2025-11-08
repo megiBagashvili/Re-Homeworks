b9e80f25ca26b9b301beefbff7d0c02524ca15e9
megiBagashvili <megibagashvili2004@gmail.com>
2025-11-08 19:13:56 +0400

    connect mongodb

--- homework1415/main.js
+++ homework1415/main.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const { logger } = require('./middlewares/logger.middleware')
 
 const expenseRouter = require('./router/expense.router')
 const factRouter = require('./router/fact.router')
+
+dotenv.config()
 
 const app = express()
 
@@ -16,6 +20,11 @@ app.get('/', (req, res) => {
 app.use('/expenses', expenseRouter)
 app.use('/', factRouter)
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
 })
