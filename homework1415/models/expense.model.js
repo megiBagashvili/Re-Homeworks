69f23e80b7f40f2b1b86e2f2c09fbeff8c714580
megiBagashvili <megibagashvili2004@gmail.com>
2025-11-08 19:20:34 +0400

    completed task 1

---
+++ homework1415/models/expense.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const expenseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        default: 'General'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Expense = mongoose.model('Expense', expenseSchema)
+
+module.exports = Expense
