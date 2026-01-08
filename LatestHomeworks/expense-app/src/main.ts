c83f5e2cf84cda5c82e8104857ae1da3600e77d8
megiBagashvili <megibagashvili2004@gmail.com>
2026-01-08 22:25:31 +0400

    install swagger and configure it

--- LatestHomeworks/expense-app/src/main.ts
+++ LatestHomeworks/expense-app/src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Expense App API')
+    .setDescription('API documentation for Users, Expenses, Products, and Statistics')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
